ff5a6d2008f6164c59720997c6b3c2ae380cc0fc
lukas <lhofmaie@hsr.ch>
2015-05-21 22:30:51 +0200

    haskell code angepasst

--- test.hs
+++ test.hs
@@ -1,5 +1,9 @@
+
 input :: [Int]
 input = [1,2,3]
 
 test_reverse :: [Int] -> Bool
 test_reverse xs = reverse (reverse xs) == xs
+
+dontchangecontext = do line <- fmap reverse getLine
+                       putStrLn $ " "
