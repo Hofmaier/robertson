2c5785286fb2564ace4965898a50ac937f85cbca
lukas <lhofmaie@hsr.ch>
2015-05-28 10:30:43 +0200

    test.hs um beispiele erweitert

--- test.hs
+++ test.hs
@@ -1,3 +1,7 @@
+x = 23
+
+showlist1 :: [t] -> String
+showlist1 x = "not implemented"
 
 input :: [Int]
 input = [1,2,3]
@@ -14,3 +18,16 @@ showlist [] = ""
 showlist (x:xs) = show x ++ showlist xs
 
 lengthtest x xs y ys = length ((x:xs) ++ ys) == length (x:xs) + (length ys)
+
+data MonoidInst = Elem1 | Elem2
+
+class Monoid m where
+  mempty :: m
+  mappend :: m -> m -> m
+  mconcat :: [m] -> m
+  mconcat = foldr mappend mempty
+
+
+instance Monoid MonoidInst where
+  mappend m1 m2 = Elem1
+  mempty = Elem1
